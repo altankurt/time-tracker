01cce70b53c730b23c149305c56ffc9e63197f72
altankurt <aaltankurt@gmail.com>
2023-12-15 05:15:40 +0300

    feat: complete project setup requirements

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCPbVvBnHJxKXP3UqeDI6B7PCq0TWIQRgs',
+  authDomain: 'time-tracker-epigra.firebaseapp.com',
+  projectId: 'time-tracker-epigra',
+  storageBucket: 'time-tracker-epigra.appspot.com',
+  messagingSenderId: '102649970067',
+  appId: '1:102649970067:web:bad5c8c43a39a152d6bc66',
+};
+
+// eslint-disable-next-line no-unused-vars
+const app = initializeApp(firebaseConfig);
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import './index.css';
+
+export default function App() {
+  return (
+    <h1 className="text-3xl text-blue-600 font-bold underline">Hello world!</h1>
+  );
+}
