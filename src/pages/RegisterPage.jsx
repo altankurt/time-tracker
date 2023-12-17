e2634784c80d90e43f0259fe2d5177ed6e5acb08
altankurt <aaltankurt@gmail.com>
2023-12-17 16:45:11 +0300

    feat: import hooks, zod, UI components

---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,15 @@
+import { useForm } from "react-hook-form";
+import { useNavigate, Link } from "react-router-dom";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../components/ui/form";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
+import { useToast } from "../components/ui/use-toast";
