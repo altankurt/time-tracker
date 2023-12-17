3bd16de64d12b4d2fe4b3b560121423d0f528d9a
altankurt <aaltankurt@gmail.com>
2023-12-17 17:24:42 +0300

    feat: signup form with all input fields and navigation links

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -60,7 +60,110 @@ const Signup = () => {
   };
 
   return (
-    <div>hi</div>
+    <main className="main-layout">
+      <section className="form-container">
+        <header className="text-center">
+          <h1 className="header-title">TimeTune</h1>
+          <p className="header-subtitle">Sign up and start your journey!</p>
+        </header>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="form-submit">
+          {/* Email Field */}
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Your email</FormLabel>
+                <FormControl>
+                  <Input className="rounded-md " placeholder="mail@example.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Username Field */}
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input className="rounded-md" placeholder="johndoe" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Password Field */}
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input 
+                    className="rounded-md"
+                    placeholder="Enter your password"
+                    type="password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Confirm Password Field */}
+          <FormField
+            control={form.control}
+            name="confirmPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Confirm password</FormLabel>
+                <FormControl>
+                  <Input
+                    className="rounded-md"
+                    placeholder="Re-enter your password"
+                    type="password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button className="form-button text-base bg-secondary hover:bg-primary" type="submit">
+            Sign up
+          </Button>
+          <p className="m-8 text-center text-xs text-dark">
+            By proceeding, you agree to our 
+            <Link className="form-link" to="/terms">{" "}
+              Terms of Service
+            </Link>{" "}
+            and 
+            <Link className="form-link" to="/policy">{" "}
+              Privacy Policy
+            </Link>
+            <span>.</span>
+          </p>
+
+          <p className="form-small-text">
+            Already have an account?{" "}
+            <Link className="form-link" to="/login">
+              Sign in
+            </Link>
+          </p>
+        </form>
+      </Form>
+      </section>
+
+    </main>
   );
 };
 
