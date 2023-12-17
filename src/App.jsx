d58ba23d1ba5947614e57d2e50226778bfb61a5d
altankurt <aaltankurt@gmail.com>
2023-12-17 06:05:32 +0300

    feat: integrate react-router, auth state management, and new page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
-import './index.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import IndexPage from './pages/IndexPage';
+import ProtectedRoute from './components/common/ProtectedRoute';
 
 export default function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const auth = getAuth();
+
+  useEffect(() => {
+    const logout = onAuthStateChanged(auth, user => {
+      setCurrentUser(user);
+    });
+
+    return logout; // Oturum izleme bittiğinde unsubscribe ol
+  }, [auth]);
+
   return (
-    <h1 className="text-3xl text-blue-600 font-bold underline">Hello world!</h1>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/index" element={<IndexPage />} />
+        
+        <Route 
+          path="/index" 
+          element={
+            <ProtectedRoute>
+              <IndexPage />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/" 
+          element={currentUser ? <Navigate replace to="/index" /> : <Navigate replace to="/login" />} 
+        />
+      </Routes>
+    </Router>
   );
 }
