771cace6dd9cb6c2c1f57dd0e145ab24519c3fd9
altankurt <aaltankurt@gmail.com>
2023-12-17 16:52:48 +0300

    feat: add zod validation schema for signup form

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -13,3 +13,13 @@ import {
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
 import { useToast } from "../components/ui/use-toast";
+
+const FormSchema = z.object({
+  email: z.string().min(1, "Email is required!").email("Invalid email!"),
+  username: z.string().min(2, "Username is required!").max(15, "Username must have less than 15 characters!"),
+  password: z.string().min(8, "Password must have more than 8 characters!"),
+  confirmPassword: z.string().min(1, "Password confirmation is required!"),
+}).refine(data => data.password === data.confirmPassword, {
+  path: ["confirmPassword"],
+  message: "Passwords do not match!",
+});
