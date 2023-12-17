cee5a78e37738375ba09cc6fdb669d12020953ca
altankurt <aaltankurt@gmail.com>
2023-12-17 11:58:03 +0300

    fix: remove unprotected /index route

--- src/App.jsx
+++ src/App.jsx
@@ -23,8 +23,6 @@ export default function App() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/index" element={<IndexPage />} />
-        
         <Route 
           path="/index" 
           element={
