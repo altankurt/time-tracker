62f5284309ea5d4ba68b07fa24be2e5e09ba002a
altankurt <aaltankurt@gmail.com>
2023-12-17 06:28:12 +0300

    feat: add ProtectedRoute function with auth and navigation

--- src/components/common/ProtectedRoute.jsx
+++ src/components/common/ProtectedRoute.jsx
@@ -2,4 +2,12 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const auth = getAuth();
 
+  return children;
+}
+
+export default ProtectedRoute;
