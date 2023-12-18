60c37e56493134e6b0503e98bd47722150675eb1
altankurt <aaltankurt@gmail.com>
2023-12-18 09:28:22 +0300

    feat: add date format function

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -72,6 +72,22 @@ const IndexPage = () => {
     setTaskDescription('');
   };
 
+  function formatDate(date) {
+    if (!date) {
+      return '';
+    }
+  
+    const d = new Date(date.toDate());
+  
+    const pad = (num) => num.toString().padStart(2, '0');
+  
+    const day = pad(d.getDate());
+    const month = pad(d.getMonth() + 1);
+    const year = d.getFullYear();
+
+  
+    return `${day}/${month}/${year}`;
+  }
 };
 
 export default IndexPage;
