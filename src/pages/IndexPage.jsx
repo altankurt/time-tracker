dae4aa53cfed7affcc527e32239446b8d4defafd
altankurt <aaltankurt@gmail.com>
2023-12-18 08:37:55 +0300

    feat: setup state and navigation

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -3,3 +3,14 @@ import { getAuth, signOut } from "firebase/auth";
 import { useNavigate } from 'react-router-dom';
 import { collection, addDoc, getDocs, deleteDoc, query, where } from "firebase/firestore";
 import { db } from '../../firebase-config';
+
+
+const IndexPage = () => {
+  const [time, setTime] = useState(0);
+  const [timerOn, setTimerOn] = useState(false);
+  const [taskDescription, setTaskDescription] = useState('');
+  const [logs, setLogs] = useState([]);
+  const navigate = useNavigate();
+};
+
+export default IndexPage;
