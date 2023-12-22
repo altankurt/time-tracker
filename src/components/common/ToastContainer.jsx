e0df8f570ba5abb3c4fbbd732eb6aa1fdc9aec91
altankurt <aaltankurt@gmail.com>
2023-12-22 15:56:35 +0300

    fix: update import path for useToast hook

--- src/components/common/ToastContainer.jsx
+++ src/components/common/ToastContainer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useToast } from '../ui/use-toast'
+import { useToast } from '../../hooks/useToast'
 
 function ToastContainer() {
   const { toasts } = useToast()
